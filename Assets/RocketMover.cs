8a7d281130eb6b9a5b412aa2ef259453e1ef6285
artcodex <artcodex@gmail.com>
2021-02-09 07:23:03 -0700

    unity_3_46

---
+++ Assets/RocketMover.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMover : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    Vector3 originalPosition;
+    Quaternion originalRotation;
+
+    [SerializeField]
+    public int RCSThrust = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.position = originalPosition;
+            transform.rotation = originalRotation;
+
+            rigidBody.velocity = Vector3.zero;
+            rigidBody.angularVelocity = Vector3.zero;
+        }
+
+        var thrust = RCSThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(thrust * Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(thrust * -Vector3.forward);
+        }
+    }
+}
